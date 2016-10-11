e30f1fc2821b98a18cdda81eb575d10346cebb6e
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-11 23:35:15 +0100

    Add initialize method to SteamValve

--- lib/steam_valve.rb
+++ lib/steam_valve.rb
@@ -3,4 +3,10 @@ require "steam_valve/steam_api_key"
 require "exceptions/api_key_missing_error"
 
 module SteamValve
+  attr_reader :api_key
+
+  def initialize
+    API_KEY = SteamAPIKey.api_key
+    puts API_KEY
+  end
 end
