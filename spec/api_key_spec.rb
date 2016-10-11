aa16a1efa52904631ed529a0ba2708b6ac7f121a
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-11 22:25:36 +0100

    Add spec to cover no API key

--- spec/api_key_spec.rb
+++ spec/api_key_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 describe SteamAPIKey do
   describe '#api_key' do
-    it 'returns the API key' do
-      SteamAPIKey.set_api_key('ABCDEF')
+    context 'with an api key' do
+      it 'returns the API key' do
+        SteamAPIKey.set_api_key('ABCDEF')
 
-      expect(SteamAPIKey.api_key).to eq('ABCDEF')
+        expect(SteamAPIKey.api_key).to eq('ABCDEF')
+      end
+    end
+
+    context 'without a key' do
+      it 'raises a ApiKeyMissingError exception' do
+        SteamAPIKey.set_api_key('')
+
+        expect(SteamAPIKey.api_key).to raise(ApiKeyMissingError)
+      end
     end
   end
 end
