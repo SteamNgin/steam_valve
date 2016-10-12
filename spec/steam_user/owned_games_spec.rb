d466c85d5a47f014041f7562f576c17d8352d3fd
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-12 17:44:18 +0100

    make steam id dynamic

--- spec/steam_user/owned_games_spec.rb
+++ spec/steam_user/owned_games_spec.rb
@@ -4,7 +4,7 @@ describe SteamValve do
   describe '#game_list' do
     context 'with a valid player_id' do
       it 'returns all of the players games' do
-        expect(SteamValve.game_list['response']).to have_key('game_count')
+        expect(SteamValve.game_list('76561198030992261')['response']).to have_key('game_count')
       end
     end
   end
