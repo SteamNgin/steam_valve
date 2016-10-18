231bcbf4492d0320b03fed4328fc25401dec8c03
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-18 20:24:42 +0100

    exctracts service type into different files

--- lib/steam_valve/player_service.rb
+++ lib/steam_valve/player_service.rb
@@ -5,10 +5,4 @@ module SteamValve
     @api_endpoint_url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{SteamAPIKey.api_key}&steamid=#{steam_id}&include_appinfo=1&format=json"
     HTTParty.get(@api_endpoint_url).to_hash
   end
-
-  def self.current_players(game_id)
-    @api_endpoint_url =  "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=#{game_id}"
-    HTTParty.get(@api_endpoint_url).to_hash
-  end
-
 end
